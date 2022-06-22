0f1808fe01e20510a0b2ac7c561b16ae480a75ed
DESKTOP-MCNF9AT\Khoa <khoa6430@gmail.com>
2022-06-22 07:07:15 +0700

    deploy 1

---
+++ src/components/pages/Services.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../../App.css';
+
+
+export default function Services(){
+    return <h1 className="services">SERVICES</h1>;
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../../App.css';
+
+
+export default function SignUp(){
+    return <h1 className="sign-up">SIGN UP</h1>;
+}
+
+
+
+
+
+
+
+
+
+
